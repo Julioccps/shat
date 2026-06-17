7782a59b5276464bf0ef8a808ea11b3f197ec39e
Julio Cesar Carvalho de Paula Souza <julio191096@gmail.com>
2026-06-16 22:36:08 -0300

    feat: core: implemented place-holder option parsing
    - Implemented basic option for deciding the port on which the chat would
      be set at

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// A private Focused Peer-to-Peer cli Chat.
+/// 
+/// Allowing users to chat privatelly in a local chat server.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli{
+    /// Defines the port on which the server is set to be hosted at
+    #[arg(short = 'H', long = "host-at-port", default_value_t = 55555)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("Port: {}", cli.port);
 }
